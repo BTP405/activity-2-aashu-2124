6cfae92279b509e8061525b54998ca81f2f9e874
aashu-2124 <131811094+aashu-2124@users.noreply.github.com>
2024-02-12 18:42:56 -0500

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,1 +1,17 @@
 def wordCount(t):
+    word_dict = {}
+
+    with open(t, 'r') as file:
+        for line_num, line in enumerate(file, start=1):
+            words = line.strip().split()
+            for word in words:
+                word = word.lower()
+                if word not in word_dict:
+                    word_dict[word] = [line_num]
+                else:
+                    word_dict[word].append(line_num)
+    return word_dict
+
+file_path = 'Activity2.txt'  
+result = wordCount(file_path)
+print(result)
